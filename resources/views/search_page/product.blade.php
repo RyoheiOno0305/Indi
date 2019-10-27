758f9993e57394a380ced410a543dab30f8c576f
root <onoryohei0305@gmail.com>
2019-10-27 08:21:30 +0000

    画像の表示とsliderの実装

---
+++ resources/views/search_page/product.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <!-- css  -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+    <!-- swiper -->
+    <link rel="stylesheet" href="css/swiper.css">
+    
+
+    
+
+
+    <title>Indi/トップページ</title>
+  </head>
+  <body>
+    <p>
+      {{$response['count']}}件見つかりました。
+    </p>
+
+    <div class="offset-md-1">
+      @foreach($response as $item)
+      
+        <!-- @foreach($item['mediumImageUrls'] as $image)
+          <ul class="slider">
+            <li><a href="#"><img src="{{$image['imageUrl']}}" alt=""></a></li>
+          </ul>
+        @endforeach  -->
+
+
+        <div class="swiper-container">
+          <div class="swiper-wrapper">
+            @foreach($item["mediumImageUrls"] as $image)
+              <div class="swiper-slide"><img src="{{$image['imageUrl']}}" alt=""></div>
+            @endforeach
+          </div>
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next"></div>
+        </div>
+      
+        
+        
+        <p class="item-title">{{$item['itemName']}}</p>
+        <p class="item-title">{{$item['catchcopy']}}</p>
+        <br>
+        <br>          
+      @endforeach
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
+    <script src="js/swiper.js"></script>
+    <script>
+      var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+      });
+    </script>
+  </body>
+</html>
